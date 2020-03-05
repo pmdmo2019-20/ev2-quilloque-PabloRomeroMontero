231406db3fdc6172243939c31fba331fb164d762
pabloromeromontero <pabloromeromontero23@gmail.com>
2020-03-05 14:07:13 +0100

    initial commit

--- app/src/main/java/es/iessaladillo/pedrojoya/quilloque/contact/ContactFragment.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/quilloque/contact/ContactFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -16,6 +17,7 @@ import es.iessaladillo.pedrojoya.quilloque.data.CALL_TYPE_MADE
 import es.iessaladillo.pedrojoya.quilloque.data.CALL_TYPE_VIDEO
 import es.iessaladillo.pedrojoya.quilloque.data.local.AppDatabase
 import es.iessaladillo.pedrojoya.quilloque.data.local.entity.Call
+import es.iessaladillo.pedrojoya.quilloque.dial.ContactFragmentAdapter
 import es.iessaladillo.pedrojoya.quilloque.recent.RecentFragmentAdapter
 import kotlinx.android.synthetic.main.contacts_fragment.*
 
@@ -24,12 +26,8 @@ class ContactFragment: Fragment(R.layout.contacts_fragment) {
 
     private val navController by lazy { findNavController() }
 
-    private val listAdapter: RecentFragmentAdapter =
-        RecentFragmentAdapter().apply {
-            addContact = {
-                viewModel.phoneNumberDial.value = viewModel.queryCall(getItemId(it)).phoneNumber
-                navController.navigate(R.id.contactCreationFragmentDestination)
-            }
+    private val listAdapter: ContactFragmentAdapter =
+        ContactFragmentAdapter().apply {
 
 
             onClickListener = {
@@ -43,7 +41,7 @@ class ContactFragment: Fragment(R.layout.contacts_fragment) {
                     Call(
                         0,
                         CALL_TYPE_MADE,
-                        viewModel.queryCall(getItemId(it)).phoneNumber,
+                        viewModel.queryContact(getItemId(it)).phoneNumber,
                         "12-05-1995",
                         "12:45"
                     )
@@ -57,7 +55,7 @@ class ContactFragment: Fragment(R.layout.contacts_fragment) {
                     Call(
                         0,
                         CALL_TYPE_VIDEO,
-                        viewModel.queryCall(getItemId(it)).phoneNumber,
+                        viewModel.queryContact(getItemId(it)).phoneNumber,
                         "12-05-1995",
                         "12:45"
                     )
@@ -82,15 +80,16 @@ class ContactFragment: Fragment(R.layout.contacts_fragment) {
     }
 
     private fun observe() {
-//        viewModel.contactList.observe(this){
-//            listAdapter.submitList(it)
-//            emptyView.visibility = if (it.isEmpty) View.VISIBLE else View.INVISIBLE
-//        }
+        viewModel.contactList.observe(this){
+            listAdapter.submitList(it)
+            emptyView.visibility = if (it.isEmpty()) View.VISIBLE else View.INVISIBLE
+        }
     }
 
 
     private fun initView() {
         setupRecyclerView()
+        emptyView.setOnClickListener { navController.navigate(R.id.contactCreationFragmentDestination) }
 
     }
 
